e9371ed9132da6bade2756f8a3211e6c0b6c2f88
ashr666 <ashr@ashr.net>
2017-02-03 10:24:57 +0200

    Added class to be shared with pattern_offset

---
+++ OffsetFinder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace pattern_create
+{
+	public class OffsetFinder
+	{
+		public OffsetFinder (){}
+
+		static char currentFirst = 'Z';
+		static char currentSecond ='z';
+		static int currentThird = 9;
+
+		private static string getNextNumber(){
+			char incCurrentFirst = currentFirst;//(char)((int)currentFirst + 1);
+			char incCurrentSecond = currentSecond;//(char)((int)currentSecond + 1);
+			int incCurrentThird = ++currentThird;
+
+			if (incCurrentThird > 9) {
+				incCurrentThird = 0;
+
+				incCurrentSecond = (char)((int)currentSecond + 1);
+
+				if (((int)incCurrentSecond) > ((int)'z')) {
+					incCurrentSecond = 'a';
+
+					incCurrentFirst = (char)((int)currentFirst + 1);
+
+					if (((int)incCurrentFirst) > ((int)'Z')) {
+						incCurrentFirst = 'A';
+					}
+				}
+			}
+
+			currentFirst = incCurrentFirst;
+			currentSecond = incCurrentSecond;
+			currentThird = incCurrentThird;
+
+			return incCurrentFirst.ToString() + incCurrentSecond.ToString() + incCurrentThird.ToString ();
+		}
+
+		public string GeneratePattern(int Length){
+			StringBuilder output = new StringBuilder ();
+
+			while (output.Length < Length) {
+				output.Append (getNextNumber ());
+			}
+
+			return (output.ToString ().Substring(0,Length));			
+		}
+
+		public int FindOffset(string Pattern, int Length){
+			throw new NotImplementedException ();
+		}
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -12,46 +12,10 @@ namespace pattern_create
 			}
 
 			int length = int.Parse (args [0]);
-			StringBuilder output = new StringBuilder ();
-
-			while (output.Length < length) {
-				output.Append (getNextNumber ());
-			}
-
-			Console.WriteLine (output.ToString ().Substring(0,length));
+			Console.WriteLine (new OffsetFinder().GeneratePattern(length));
 		}
 
-		static char currentFirst = 'Z';
-		static char currentSecond ='z';
-		static int currentThird = 9;
 
-		private static string getNextNumber(){
-			char incCurrentFirst = currentFirst;//(char)((int)currentFirst + 1);
-			char incCurrentSecond = currentSecond;//(char)((int)currentSecond + 1);
-			int incCurrentThird = ++currentThird;
-
-			if (incCurrentThird > 9) {
-				incCurrentThird = 0;
-
-				incCurrentSecond = (char)((int)currentSecond + 1);
-					
-				if (((int)incCurrentSecond) > ((int)'z')) {
-					incCurrentSecond = 'a';
-
-					incCurrentFirst = (char)((int)currentFirst + 1);
-						
-					if (((int)incCurrentFirst) > ((int)'Z')) {
-						incCurrentFirst = 'A';
-					}
-				}
-			}
-
-			currentFirst = incCurrentFirst;
-			currentSecond = incCurrentSecond;
-			currentThird = incCurrentThird;
-
-			return incCurrentFirst.ToString() + incCurrentSecond.ToString() + incCurrentThird.ToString ();
-		}
 
 		private static void showUsage(){
 			Console.WriteLine ("pattern_create stringlength (pattern_create 1024)");
